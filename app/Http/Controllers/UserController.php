15bc20e4d7d7d432400161e647401610dd0b28a6
Bilobatetonto <cris_misterio10@hotmail.com>
2022-10-24 17:34:38 -0600

    creacion de migracion y seeder

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\User;
 
 class UserController extends Controller
 {
@@ -14,7 +15,8 @@ class UserController extends Controller
     public function index()
     {
         //
-        echo 'Achoy perra';
+        // echo 'Achoy perra';
+        return User::all();
     }
 
     /**
